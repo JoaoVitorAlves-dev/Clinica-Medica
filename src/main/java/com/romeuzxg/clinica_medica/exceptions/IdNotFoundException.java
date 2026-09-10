fd4fba510a9c19b43924a7e8a63506c91bfe478f
JoaoVitorAlves-dev <ytmarcos43@gmail.com>
2026-09-10 18:54:17 -0300

    feat: Adiciona pacote exceptions: classes GlobalHandlerException e IdNotFoundException.

---
+++ src/main/java/com/romeuzxg/clinica_medica/exceptions/IdNotFoundException.java
@@ -0,0 +1,9 @@
+package com.romeuzxg.clinica_medica.exceptions;
+
+public class IdNotFoundException extends RuntimeException {
+
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+
+}
